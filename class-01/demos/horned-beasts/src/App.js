32a6ad38344ebd0ee7f6b5358b12ce96897370ec
JB-Tellez <jb.tellez@gmail.com>
2021-09-13 12:06:40 -0700

    adds class 01 overview and demo

---
+++ class-01/demos/horned-beasts/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import { Component } from 'react';
+import Header from './Header.js';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main message={'Hi there'} />
+      <Footer />
+    </div>
+  );
+}
+
+
+class Footer extends Component {
+
+  render() {
+    return <h1>I am a footer</h1>
+  }
+}
+
+class Main extends Component {
+  // needs to know the title pass in somehow.....
+  render() {
+    return (
+      <>
+        <h1>I am Main my message is {this.props.message}</h1>
+        <HornedBeast title={'Rhino'} />
+      </>
+    )
+  }
+}
+
+class HornedBeast extends Component {
+  render() {
+    return <h1>I am a horned beast with title of {this.props.title}</h1>
+  }
+}
+export default App;
