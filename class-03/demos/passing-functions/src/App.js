6c1d50c02b738622abd6484d5006c0da4a68290e
JB-Tellez <jb.tellez@gmail.com>
2021-09-15 09:24:16 -0700

    adds class 3 overview and demo starter

---
+++ class-03/demos/passing-functions/src/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+
+function App() {
+  return (
+    <Container>
+
+      <h1>Coming Soon</h1>
+
+
+    </Container>
+  );
+}
+
+
+export default App;
